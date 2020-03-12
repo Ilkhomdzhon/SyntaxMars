1fe1d681d731acb3fab7f3d257def68a9c767e81
ilhomahmadov3@gmail.com <ilkhomdzhonishonkulov@Ilkhomdzhons-MacBook-Air.local>
2020-03-11 21:54:02 -0400

    First class added

--- src/Test111.java
+++ src/Test111.java
@@ -1,9 +1,12 @@
 
-public class Test1 {
+public class Test111 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hello World");
+		
+		 
 	}
 
 }
