ca3472b276e5e289ec37d80fed633b31a925f52e
ilhomahmadov3@gmail.com <ilkhomdzhonishonkulov@Ilkhomdzhons-MacBook-Air.local>
2020-03-17 21:19:17 -0400

    is it added

--- src/com/syntax/test01/Test01.java
+++ src/com/syntax/test01/Test01.java
@@ -5,6 +5,12 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		boolean isRaining= true;
+		
+	if (isRaining) {
+		System.out.println("Then it is raining ");
+	}
+		 
 	}
 
 }
